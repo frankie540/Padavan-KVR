555d7047cbdb8b41cc26a236039e87c6e6ca755b
frankie540 <117900565+frankie540@users.noreply.github.com>
2025-11-26 16:35:29 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -1002,8 +1002,9 @@ struct nvram_pair router_defaults[] = {
 #if defined(BOARD_ROLE_REPEATER)
 	{ "sw_mode", "3" },
 #else
-	{ "sw_mode", "1" },
+	{ "sw_mode", "3" },
 #endif
+#修改此处为3AP模式，1为路由器模式。
 
 	{ "telnetd", "0" },
 	{ "sshd_enable", "1" },
